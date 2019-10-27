73f2cb1c783fb08bae621302dab2058754c999fe
birdman98 <piotrmatras21@gmail.com>
2019-10-27 21:51:35 +0100

    added basic methods to View class

--- TextFilesCompressor/src/textfilescompressor/view/View.java
+++ TextFilesCompressor/src/textfilescompressor/view/View.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textfilescompressor.view;
+
+import java.util.Scanner;
 
 /**
  *
  * @author Piotr Matras
  */
-public class View {
+public class View {    
+     
+    private final Scanner scanner;
+    
+    public View() {
+        
+        this.scanner = new Scanner(System.in);
+    }
+    
+    public void displayWelcomeScreen() {
+        
+        System.out.println("Welcome in text files compressor application.");
+    }
+    
+    public void clearScreen() {
+        
+        System.out.print("\033[H\033[2J");  
+        System.out.flush(); 
+    }
     
 }
