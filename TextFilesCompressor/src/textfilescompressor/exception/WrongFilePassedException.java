7e4d76c85884934b43e75beb6fa8da8cff5f3ef0
birdman98 <piotrmatras21@gmail.com>
2019-10-27 23:07:01 +0100

    added own WrongFilePassedException exception

---
+++ TextFilesCompressor/src/textfilescompressor/exception/WrongFilePassedException.java
@@ -0,0 +1,14 @@
+package textfilescompressor.exception;
+
+/**
+ *
+ * @author Piotr Matras
+ */
+public class WrongFilePassedException extends Exception {
+    
+    public WrongFilePassedException(String exceptionMessage) {
+        
+        super(exceptionMessage);
+    }
+    
+}
