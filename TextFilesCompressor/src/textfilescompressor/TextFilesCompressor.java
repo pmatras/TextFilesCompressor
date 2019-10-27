06d44c793e27725c3a9378f62501b2eb63893ca0
birdman98 <piotrmatras21@gmail.com>
2019-10-27 22:41:28 +0100

    added starting main method of Controller class in main class of program

--- TextFilesCompressor/src/textfilescompressor/TextFilesCompressor.java
+++ TextFilesCompressor/src/textfilescompressor/TextFilesCompressor.java
@@ -1,5 +1,7 @@
 package textfilescompressor;
 
+import textfilescompressor.controller.Controller;
+
 /**
  *
  * @author Piotr Matras
@@ -8,9 +10,14 @@ public class TextFilesCompressor {
 
     /**
      * @param args the command line arguments
+     * -c / -d : choose mode - -c means compress, -d means decompress
+     * -i : after this arg please specify input file name
+     * -o : after this arg please specify outpu file name
      */
     public static void main(String[] args) {
-       
+        
+       Controller controller = new Controller();
+       controller.startProgram(args);       
     }
     
 }
