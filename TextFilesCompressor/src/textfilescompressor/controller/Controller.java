826675a8050fddea2885d2dbb7c6657455d7d8e2
birdman98 <piotrmatras21@gmail.com>
2019-10-27 21:53:42 +0100

    added parsing CMD arguments

--- TextFilesCompressor/src/textfilescompressor/controller/Controller.java
+++ TextFilesCompressor/src/textfilescompressor/controller/Controller.java
@@ -1,14 +1,36 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textfilescompressor.controller;
+
+import textfilescompressor.view.View;
 
 /**
  *
  * @author Piotr Matras
  */
 public class Controller {
+    
+    private String inFileName;
+    private String outFileName;
+    private String mode;
+      
+    private void setArguments(String[] args) {
+        
+        if(args[0].equals("-c")) {
+            
+            this.mode = "compress";
+        } else if(args[1].equals("-d")) {
+            
+            this.mode = "decompress";
+        }
+        
+        if(args[1].equals("-i")) {
+            
+            this.inFileName = args[2];
+        } 
+        if(args[3].equals("-o")) {
+            
+            this.outFileName = args[4];
+        }
+        
+    }
     
 }
