e9b85084fdd65a867473261be37d150382026dd5
birdman98 <piotrmatras21@gmail.com>
2019-11-02 15:47:07 +0100

    added method to get run arguments from user, if don't passed as command line arguments

--- TextFilesCompressor/src/textfilescompressor/view/View.java
+++ TextFilesCompressor/src/textfilescompressor/view/View.java
@@ -5,9 +5,23 @@ import java.util.Scanner;
 /**
  *
  * @author Piotr Matras
- * @version 1.0
+ * @version 1.0.1
  */
-public class View {    
+public class View {  
+    /**
+     * ArgsCount - constant variable which defines default number of args to get from user
+     * (if not passed as CMD args)
+     */
+    private final int ArgsCount = 3;
+    /**
+     * scanner - Scanner object to get input from user
+     */
+    private final Scanner scanner;
+    
+    public View() {
+        
+        this.scanner = new Scanner(System.in);
+    }
     
     public void displayWelcomeScreen() {
         
@@ -29,5 +43,44 @@ public class View {
         
         System.out.println(message);
     }
+    /**
+     * 
+     * @return array of Strings which stores arguments from user 
+     * Method gets arguments from user, if arguments weren't specified as CMD args
+     */
+    public String[] getArgsFromUser() {
+        
+        View view = new View();        
+        String[] args = new String[ArgsCount];
+        
+        view.displayMessage("Please enter input file name with its extension:");
+        String usersInFileName = scanner.next();
+        view.displayMessage("Please enter output file name with its extension:");
+        String usersOutFileName = scanner.next();
+        view.displayMessage("Please enter mode of program\n1. compress,\n2.decompress.: ");
+        int usersMode = scanner.nextInt();
+        
+        args[0] = usersInFileName;
+        args[1] = usersOutFileName;
+        switch(usersMode) {
+            case 1: {
+                
+                args[2] = Mode.compress.toString();
+                break;
+            }
+            case 2: {
+                
+                args[2] = Mode.decompress.toString();
+            }
+        }
+        
+        view.clearScreen();
+        return args;
+    }
     
+     public enum Mode {
+        
+        compress,
+        decompress 
+    }    
 }
