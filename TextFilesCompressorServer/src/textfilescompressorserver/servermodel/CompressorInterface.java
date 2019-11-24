ac79e3c98a79f03c0ef4a0657bed931f3d32a4a7
birdman98 <piotrmatras21@gmail.com>
2019-11-24 18:38:18 +0100

    added Compressor interface class

---
+++ TextFilesCompressorServer/src/textfilescompressorserver/servermodel/CompressorInterface.java
@@ -0,0 +1,14 @@
+package textfilescompressorserver.servermodel;
+
+import java.io.IOException;
+
+/**
+ *
+ * @author Piotr Matras
+ * @version 1.0
+ */
+public interface CompressorInterface {
+    
+    public boolean compressFile() throws WrongFilePassedException, IOException;
+    public boolean decompressFile() throws WrongFilePassedException, IOException;     
+}
