3d3460c9d8e779cee8d674ab81a9713caf24e4ec
birdman98 <piotrmatras21@gmail.com>
2019-11-24 19:54:17 +0100

    added constructor for SingleServiceConnection class

--- TextFilesCompressorServer/src/textfilescompressorserver/server/SingleServiceConnection.java
+++ TextFilesCompressorServer/src/textfilescompressorserver/server/SingleServiceConnection.java
@@ -1,8 +1,11 @@
 package textfilescompressorserver.server;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -16,6 +19,19 @@ public class SingleServiceConnection implements Closeable {
     private Socket socket;
     private BufferedReader clientsInput;
     private PrintWriter clientsOutput;
+    
+    public SingleServiceConnection(Socket socket) throws IOException {
+        
+        this.socket = socket;
+        this.clientsOutput = new PrintWriter(
+            new BufferedWriter(
+            new OutputStreamWriter(
+            this.socket.getOutputStream())), true);
+        
+        this.clientsInput = new BufferedReader(
+            new InputStreamReader(
+            this.socket.getInputStream()));
+    }
 
     @Override
     public void close() throws IOException {
