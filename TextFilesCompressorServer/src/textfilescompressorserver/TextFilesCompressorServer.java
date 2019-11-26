1da1f82ab003dcc9a56f4a391e46ccc618fe6425
birdman98 <piotrmatras21@gmail.com>
2019-11-26 22:39:06 +0100

    main() method of application

--- TextFilesCompressorServer/src/textfilescompressorserver/TextFilesCompressorServer.java
+++ TextFilesCompressorServer/src/textfilescompressorserver/TextFilesCompressorServer.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textfilescompressorserver;
+
+import textfilescompressorserver.servercontroller.ServerController;
 
 /**
  *
- * @author Birdman
+ * @author Piotr Matras
+ * @version 1.0
  */
 public class TextFilesCompressorServer {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        
+        while(true) {
+            ServerController controller = new ServerController();
+            controller.run();
+        }    
     }
-    
 }
