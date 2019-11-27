bd5b07c666f8c0f05f0cb7c9eaeb97eb21938ce6
birdman98 <piotrmatras21@gmail.com>
2019-11-27 20:06:42 +0100

    changed javadoc

--- TextFilesCompressorServer/src/textfilescompressorserver/servercontroller/Status.java
+++ TextFilesCompressorServer/src/textfilescompressorserver/servercontroller/Status.java
@@ -4,12 +4,9 @@ package textfilescompressorserver.servercontroller;
  *
  * @author Piotr Matras
  * @version 1.0
- */
-
-/**
- * 
  * Possible statuses when client communicate with server
  */
+
 enum Status {
     ACCEPTED,
     ERROR,
