c19f4726469b3874051d61e0499f063fe7fb97b3
birdman98 <piotrmatras21@gmail.com>
2019-11-30 15:35:18 +0100

    added method for displaing message for client

--- TextFilesCompressorClient/src/textfilescompressorclient/view/ClientsInterface.java
+++ TextFilesCompressorClient/src/textfilescompressorclient/view/ClientsInterface.java
@@ -1,10 +1,16 @@
 package textfilescompressorclient.view;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Piotr Matras
  * @version 1.0
  */
 public class ClientsInterface {
-    
+        
+    public void printMessageForClient(String message) {
+        
+        System.out.println(message);
+    }    
 }
