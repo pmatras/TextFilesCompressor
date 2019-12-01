4d35777163f6a1dc68fa3c225506e6d9339276ef
birdman98 <piotrmatras21@gmail.com>
2019-12-01 16:28:15 +0100

    main() method edited

--- TextFilesCompressorClient/src/textfilescompressorclient/TextFilesCompressorClient.java
+++ TextFilesCompressorClient/src/textfilescompressorclient/TextFilesCompressorClient.java
@@ -1,13 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package textfilescompressorclient;
+
+import textfilescompressorclient.controller.ClientServerFlowGuard;
 
 /**
  *
- * @author Birdman
+ * @author Piotr Matras
+ * @version 1.0
  */
 public class TextFilesCompressorClient {
 
@@ -15,7 +13,8 @@ public class TextFilesCompressorClient {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+        
+        ClientServerFlowGuard controller = new ClientServerFlowGuard();
+        controller.runClient();
+    }    
 }
